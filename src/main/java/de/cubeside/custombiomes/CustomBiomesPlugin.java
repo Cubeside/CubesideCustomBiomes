dd2f4cb44e75a49ab6c8af8a0b018a6ca0541a4c
Brokkonaut <hannos17@gmx.de>
2023-02-09 07:56:24 +0100

    add blue_grove

--- src/main/java/de/cubeside/custombiomes/CustomBiomesPlugin.java
+++ src/main/java/de/cubeside/custombiomes/CustomBiomesPlugin.java
@@ -13,5 +13,6 @@ public class CustomBiomesPlugin extends JavaPlugin {
         nmsUtils.getBiomeUtils().registerCustomBiome(new NamespacedKey("cubeside", "cherry_grove"), 0.5f, 0.8f, Precipitation.RAIN, null, 0x69fbf9, null, 0x86fefb, 0xFF8FF9, 0xabff97); // 0xffa0fb
         nmsUtils.getBiomeUtils().registerCustomBiome(new NamespacedKey("cubeside", "cherry_blossom_valley"), 0.5f, 0.8f, Precipitation.RAIN, 0xA5D3CE, 0x3CB599, 0x1E866F, 0x64B3A9, 0xF4828C, 0x93C13C);
         nmsUtils.getBiomeUtils().registerCustomBiome(new NamespacedKey("cubeside", "red_maple_woods"), 0.5f, 0.8f, Precipitation.RAIN, 0xEFEABE, 0xA0BBF1, 0x6464F0, 0x99C7CA, 0x702822, 0xFF9C35);
+        nmsUtils.getBiomeUtils().registerCustomBiome(new NamespacedKey("cubeside", "blue_grove"), 0.5f, 0.8f, Precipitation.RAIN, 0xBF9ED1, 0x51ECB0, 0x3DC7A4, 0xDFB9F3, 0x66F0EB, 0x69D3B3);
     }
 }
